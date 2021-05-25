bb3b7d008e8d29ae6d589aec9f09c0c372d68fed
franeldev <franel4u@gmail.com>
2021-05-25 22:18:19 +0100

    setup.js file created

---
+++ src/database/setup.js
@@ -0,0 +1,17 @@
+// SETUP MONGOOSE
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost:27017/zuribook';
+
+module.exports = () => {
+  mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }, (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('database connection successful')
+    }
+  })
+}
