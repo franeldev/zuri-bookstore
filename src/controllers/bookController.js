c88509c30a7680084b4d8a0ee64e2185e5f1171e
franeldev <franel4u@gmail.com>
2021-05-25 22:17:53 +0100

    bookController.js moved into the src dir

---
+++ src/controllers/bookController.js
@@ -0,0 +1,84 @@
+const Book = require('../models/book');
+
+exports.createNewBook = (req, res) => {
+  // retieve new book details from req.body
+  Book.create({
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description,
+    category: req.body.category,
+    purchaseCount: req.body.purchaseCount,
+    imageUrl: req.body.imageUrl,
+    tags: req.body.tags,
+    color: req.body.color
+  }, (err, newBook) => {
+    if(err) {
+      return res.status(500).json({ message: err })
+    } else {
+      return res.status(200).json({ message: "new book created", newBook })
+    }
+  })
+  // create a new book and save to db
+  // send response to client
+}
+
+exports.fetchBooks = (req, res) =>{
+  // fetch all books
+  Book.find({}, (err, books) => {
+    if (err) {
+      return res.status(500).json({ message: err })
+    } else {
+      return res.status(200).json({ books })
+    }
+  })
+  // send response tom client
+}
+
+exports.fetchSingleBook = (req, res) =>{
+  Book.findById( req.params.id, (err, book) =>{
+    if (err) {
+      return res.status(500).json({ message: err })
+    }
+    else if (!book) {
+      return res.status(404).json({ message: "book not found" })
+    }
+     else {
+      return res.status(200).json({ book })
+    }
+  })
+}
+
+exports.updateSingleBook = (req, res) =>{
+  Book.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    category: req.body.category
+  }, (err, book) => {
+    if (err) {
+      return res.status(500).json({ message: err })
+    } else if (!book) {
+      return res.status(404).json({ message: "book not found" })
+    } else {
+      book.save((err, savedBook) =>{
+        if (err) {
+          return res.status(400).json({ message:err })
+        } else {
+          return res.status(200).json({ message: "bookk updated successfully" })
+        }
+      })
+    }
+  })
+}
+
+exports.deleteSingleBook = (req, res) =>{
+  Book.findByIdAndDelete(req.params.id, (err, book) =>{
+    if (err) {
+      return res.status(500).json({ message: err })
+    }
+    else if (!book) {
+      res.status(404).json({ message: "book was not found" })
+    }
+    else {
+      return res.status(200).json({ message: "book deleted successfully" })
+    }
+  })
+}
