2f472782db8a6010e1eae290dc32231c631040c5
franeldev <franel4u@gmail.com>
2021-05-31 23:22:48 +0100

    controllers authConroller.js file updates

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,11 @@ const User = require('../models/users');
 exports.registerNewUser = (req, res) {
   // fetch user details from req body
   // check if a user with this username exists
-  User.findOne({ username: req.body.username })
+  User.findOne({ username: req.body.username }, (err, existingUser) => {
+    if(err) {
+      return res.status(500).json({ err })
+    }
+  })
   // create a new user
   // hash user passsword
   // save password to db
