add49c7de4586fb3b130eb2ce39b004d97133933
franeldev <franel4u@gmail.com>
2021-05-31 20:50:42 +0100

    models user.js file updates

--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const userSchema = new Schema({
-  firstname:
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 })
