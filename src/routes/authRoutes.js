65b5cc23e265250c3db9c70adf35dbf8978460f3
franeldev <franel4u@gmail.com>
2021-06-01 23:46:23 +0100

    authRoutes.js file updates

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,3 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const AuthController = require('../controllers/authController');
+
+router.post('/signup', AuthController.registerNewUser);
+
+module.exports = router;
