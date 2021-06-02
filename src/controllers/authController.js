3d15b981ad73990bb149c417fdfaf3ef3d41994c
franeldev <franel4u@gmail.com>
2021-06-02 19:23:16 +0100

    controllers authConroller.js file updates

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -72,7 +72,7 @@ exports.loginUser = (req, res) => {
     if(!foundUser) {
       return res.status(401).json({ message: "incorrect username" })
     } else {
-      return res.status(200).json({ message: "foundUser" })
+      return res.status(200).json({ foundUser })
     }
   })
   // check if password is correct
