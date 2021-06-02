867e2bc83d9a920c27d6176d3cf34ebf3e672a8b
franeldev <franel4u@gmail.com>
2021-06-02 23:48:14 +0100

    admin.js file updates

--- src/seeders/admin.js
+++ src/seeders/admin.js
@@ -1,5 +1,6 @@
 const User = require('../models/users');
 
 exports.seedAdmin = () => {
-  // check if there is 
+  // check if there is an admin account
+  // if there is none, create an admin account
 }
