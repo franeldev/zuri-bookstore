a8b8577b44a4f854c33061450571e7389a8ce018
franeldev <franel4u@gmail.com>
2021-06-02 23:55:18 +0100

    admin.js file updates

--- src/seeders/admin.js
+++ src/seeders/admin.js
@@ -2,5 +2,16 @@ const User = require('../models/users');
 
 exports.seedAdmin = () => {
   // check if there is an admin account
-  // if there is none, create an admin account
+  User.findOne({ role: "admin" }, (err, admin) => {
+    if(err) throw err;
+    if(admin) {
+      return "admi account already exists"
+    }
+    // if there is none, create an admin account
+    User.create({
+      firstName: "Book",
+      lastName: "Goblin",
+      username: "bookgoblin"
+    })
+  })
 }
