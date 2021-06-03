cac2eec05641a4054d203fe5c3b03223f1aeacc7
franeldev <franel4u@gmail.com>
2021-06-03 16:35:16 +0100

    authController.js updates

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -18,7 +18,8 @@ exports.registerNewUser = (req, res) => {
     User.create({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
-      username: req.body.username
+      username: req.body.username,
+      role: foundUser.role
     }, (err, newUser) => {
       if(err) {
         return res.status(500).json({ err })
