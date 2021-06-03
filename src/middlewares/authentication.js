0af8712d9ba5ca0aa7f8b7103dfabb4e4ef1d257
franeldev <franel4u@gmail.com>
2021-06-03 18:40:19 +0100

    authController.js updates

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -4,12 +4,12 @@ const secret = "verySecureSECRET";
 exports.authenticateUser = (req, res, next) => {
   // check if there is an authorization login
   if (!req.headers.authorization) {
-    return res.status(401).json({ message: "authorization header required" })
+      return res.status(401).json({ message: "authorization header required" })
   }
   let splittedHeader = req.headers.authorization.split(' ');
   console.log(splittedHeader);
   if (splittedHeader[0] !== "Bearer") {
-    return res.status(401).json({ message: "authorization format is Bearer <token>" });
+      return res.status(401).json({ message: "authorization format is Bearer <token>" });
   }
   
   let token = splittedHeader[1];
@@ -19,12 +19,15 @@ exports.authenticateUser = (req, res, next) => {
   jwt.verify(token, secret, (err, decodedToken) => {
     if (err) return res.status(500).json({ err });
     if (!decodedToken) {
-      return res.status(401).json({ message: "invallid authorization token, please login" });
+        return res.status(401).json({ message: "invallid authorization token, please login" });
     }
     // and if valid, allow user to continue with request
     console.log(decodedToken);
+    req.user = decodedToken;
     next();
 
   })
 
 }
+
+exports.checkIfAdmin = (req, rees, next) => {}
