ce6f90324c3e6791c2b2951c62ee7218bde7bf82
franeldev <franel4u@gmail.com>
2021-06-03 15:02:38 +0100

    bookRoutes.js file updates

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const BookCtlr = require('../controllers/bookController');
+const { authenticateUser } = require('../m')
 
 // POST request to /books to create a new book
 router.post('/books', authenticateUser, BookCtlr.createNewBook)
