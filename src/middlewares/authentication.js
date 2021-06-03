b3e7f582b1db23315479fae3b4cf53d4b7ca3005
franeldev <franel4u@gmail.com>
2021-06-03 15:31:27 +0100

    authentication.js file updates

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,8 +1,19 @@
+const jwt = require('jsonwebtoken');
+const secret = "verySecureSECRET";
+
 exports.authenticateUser = (req, res, next) => {
   // check if there is an authorization login
   if (!req.headers.authorization) {
     return res.status(401).json({ message: "authorization header required" })
   }
+  let splittedHeader = req.headers.authorization.split(' ');
+  console.log(splittedHeader);
+  if (splittedHeader[0] !== "Bearer") {
+    return res.status(401).json({ message: "authorization format is Bearer <token>" });
+  }
+  let token
+
+
   next()
   // decode token
   // check if valid
