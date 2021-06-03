d0fb4340f4485353a70772d3a8b6bd96c219116d
franeldev <franel4u@gmail.com>
2021-06-03 15:11:54 +0100

    authentication.js file updates

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,11 @@
+exports.authenticateUser = (req, res, next) => {
+  // check if there is an authorization login
+  if (!req.headers.authorization) {
+    return res.status(401).json({ message: "authorization header required" })
+  }
+  next()
+  // decode token
+  // check if valid
+  // allow user to continue request
+}
+const
