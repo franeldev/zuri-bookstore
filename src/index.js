91f14995629be6825fb540ebea9a5de6aa628cb6
franeldev <franel4u@gmail.com>
2021-06-04 16:13:28 +0100

    index.js file updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+require('dotenv').config();
+const port = process.env.PORT;
 const dbSetup = require('./database/setup');
 
 // REQUIRE ROUTES
@@ -8,7 +9,7 @@ const bookRoutes = require('./routes/bookRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 // SEEDERS
-const {seedAdmin} = require('./seeders/admin');
+const { seedAdmin } = require('./seeders/admin');
 // console.log(seedAdmin());
 
 app.use(express.json());
@@ -19,4 +20,4 @@ dbSetup();
 app.use('/auth', authRoutes)
 app.use(bookRoutes)
 
-app.listen(port, ()=>console.log(`app listening on port ${port}`));
+app.listen(port, () => console.log(`app listening on port ${port}`));
